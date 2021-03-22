03adeef0c81f5eee9835c0fdf947c845e151e616
Kevin Kim <jhkevin21@gmail.com>
2021-03-22 17:44:48 -0600

    add image, create components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import './App.css';
+import Post from './Post';
 
 function App() {
   return (
-    <div className='App'>
+    <div className='app'>
+      <div className='app__header'>
+        <img
+          className='app__headerImage'
+          src='https://1000logos.net/wp-content/uploads/2017/02/Instagram-Logo.png'
+          alt='logo'
+        />
+      </div>
+      {/* Header */}
       <h1>Test</h1>
+
+      <Post
+        username='cleverqazi'
+        caption='WOW it works'
+        imageUrl='https://www.andreasreiterer.at/wp-content/uploads/2017/11/react-logo-825x510.jpg'
+      />
+      <Post
+        username='kevin'
+        caption='DOPE'
+        imageUrl='https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png'
+      />
+      <Post
+        username='jaehan'
+        caption='This is a fun project'
+        imageUrl='https://image.shutterstock.com/image-vector/js-logo-monogram-emblem-style-260nw-1715326756.jpg'
+      />
+
+      {/* Posts */}
+      {/* Posts */}
     </div>
   );
 }
